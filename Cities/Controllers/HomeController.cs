f265dc43c1880c9d39e7379579f41d5a7daaf616
mclaughlinpeter <p.f.mclaughlin@hotmail.co.uk>
2018-06-27 18:25:14 -0400

    Add Edit() method to Home Controller and apply asp-for tag helper

--- Cities/Controllers/HomeController.cs
+++ Cities/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using Cities.Models;
+using System.Linq;
 
 namespace Cities.Controllers
 {
@@ -14,9 +15,12 @@ namespace Cities.Controllers
 
         public ViewResult Index() => View(repository.Cities);
 
+        public ViewResult Edit() => View("Create", repository.Cities.First());
+
         public ViewResult Create() => View();
 
         [HttpPost]
+        [ValidateAntiForgeryToken]
         public IActionResult Create(City city)
         {
             repository.AddCity(city);
