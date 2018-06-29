31875a394aa579d7b8c036ebec12881d0827f586
mclaughlinpeter <p.f.mclaughlin@hotmail.co.uk>
2018-06-29 01:50:29 -0400

    Add display name and format string to model

--- Cities/Models/City.cs
+++ Cities/Models/City.cs
@@ -1,9 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Cities.Models
 {
     public class City
     {
+        [Display(Name = "City")]
         public string Name { get; set; }
         public string Country { get; set; }
+        
+        [DisplayFormat(DataFormatString = "{0:F2}", ApplyFormatInEditMode = true)]
         public int? Population { get; set; }
     }
 }
