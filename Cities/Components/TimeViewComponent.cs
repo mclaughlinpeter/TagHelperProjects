1ec80ab3ace3478aa2aac8b958535f0eea68675a
mclaughlinpeter <p.f.mclaughlin@hotmail.co.uk>
2018-07-01 13:23:51 -0400

    Add TimeViewComponent.cs with Default view and apply in _Layout.cshtml

---
+++ Cities/Components/TimeViewComponent.cs
@@ -0,0 +1,13 @@
+using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Cities.Components
+{
+    public class TimeViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View(DateTime.Now);
+        }
+    }
+}
